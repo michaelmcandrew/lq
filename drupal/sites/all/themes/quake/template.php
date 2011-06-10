8e6bc49b2932dd16abf0e769ac4975d4e69b841d
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-06-10 10:05:07 +0100

    front page and views have their own style sheet

--- drupal/sites/all/themes/quake/template.php
+++ drupal/sites/all/themes/quake/template.php
@@ -1,10 +1,23 @@
 <?php
 function quake_preprocess_page($variables){
-    if(!isset($variables['node']->type)){
+	if($variables['is_front']){
+		$sheet='front';
+	}elseif(isset($variables['node']->type)){
+ 		$sheet=$variables['node']->type;
+	}else{
+		$pathBasedCSS = array(
+			'events'=>'event',
+			'news'=>'news',
+			'map'=>'meeting'
+			'meeting'=>'meeting'
+		);
+		$firstPageArgument = arg(0);
+		$sheet = $pathBasedCSS[$firstPageArgument];
+	}
+	if(!isset($sheet)){
 		return;
 	}
-	$node_type=$variables['node']->type;
-	$stylesheet=drupal_get_path('theme', 'quake').'/color.'.$node_type.'.css';
+	$stylesheet=drupal_get_path('theme', 'quake').'/color.'.$sheet.'.css';
 	drupal_add_css($stylesheet);
 }
 ?>
