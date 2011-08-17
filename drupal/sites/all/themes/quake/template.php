9ccae1c807d78d92796d205ece1413927aed1810
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-08-17 13:09:41 +0100

    reapplying a change to set front as default style that got lost in the git matrix

--- drupal/sites/all/themes/quake/template.php
+++ drupal/sites/all/themes/quake/template.php
@@ -1,8 +1,7 @@
 <?php
 function quake_preprocess_page($variables){
-	if($variables['is_front']){
-		$sheet='front';
-	}elseif(isset($variables['node']->type)){
+	$sheet='front';
+	if(isset($variables['node']->type)){
  		$sheet=$variables['node']->type;
 	}else{
 		$firstPageArgument = arg(0);
@@ -15,9 +14,6 @@ function quake_preprocess_page($variables){
 		if(array_key_exists($firstPageArgument, $pathBasedCSS)){
 			$sheet = $pathBasedCSS[$firstPageArgument];			
 		}
-	}
-	if(!isset($sheet)){
-		return;
 	}
 	$stylesheet=drupal_get_path('theme', 'quake').'/color.'.$sheet.'.css';
 	drupal_add_css($stylesheet);
